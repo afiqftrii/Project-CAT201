8768cecda5c80a4f0634854a87c261aeed886c39
muhammadhanafi504061 <muhammadhanafi504061@gmail.com>
2026-01-11 05:54:56 +0800

    Adjust CorsFilter.java

--- backend/src/main/java/com/usmemart/filters/CorsFilter.java
+++ backend/src/main/java/com/usmemart/filters/CorsFilter.java
@@ -1,7 +1,7 @@
 package com.usmemart.filters;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 public class CorsFilter implements Filter {
